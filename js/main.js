5426aa760b822bbbf9a7026a542f165c6343596d
mansigangrade89 <64583600+mansigangrade89@users.noreply.github.com>
2020-05-04 22:29:50 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// Typing letter
+
+var typed = new Typed('.typing', {
+    strings: ["Cloud Developer", "Hardware Integrator", "Artist", "Sketcher","Cloud Developer"],
+    loop: true,
+    typeSpeed: 40,
+    backSpeed: 40,
+
+});
